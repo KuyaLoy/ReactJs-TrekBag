41935662b613f06758e72a7536d5a30bab0a7876
Robin Tapiru <robintapiru0894@gmail.com>
2024-07-27 03:25:45 +0400

    Project Structure (Reusability)

---
+++ src/component/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        &copy; 2024. Copyright by <a href="#">KuyaLoy</a>
+      </small>
+      <p>
+        Version <strong>1</strong>
+      </p>
+    </footer>
+  );
+}
