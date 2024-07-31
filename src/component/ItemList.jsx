3abd09419a62fc577b278da7a074c4b144d34625
Robin Tapiru <robintapiru0894@gmail.com>
2024-07-31 19:53:36 +0400

    React Pattern for Updating State, naming props And Empty View (Conditional Rendering Pitfalls

--- src/component/ItemList.jsx
+++ src/component/ItemList.jsx
@@ -1,17 +1,20 @@
+import EmptyView from "./EmptyView";
+
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
   return (
-    <ul>
+    <ul className="item-list">
+      {items.length === 0 && <EmptyView />}
       {items.map((item) => {
         return (
           <Item
             key={item.id}
             item={item}
-            handleDeleteItem={handleDeleteItem}
-            handleToggleItem={handleToggleItem}
+            onDeleteItem={handleDeleteItem}
+            onToggleItem={handleToggleItem}
           />
         );
       })}
@@ -19,19 +22,19 @@ export default function ItemList({
   );
 }
 
-function Item({ item, handleDeleteItem, handleToggleItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li className="item">
       <label>
         <input
-          onChange={() => handleToggleItem(item.id)}
+          onChange={() => onToggleItem(item.id)}
           type="checkbox"
           checked={item.packed}
         />
         {item.name}
       </label>
 
-      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
